fc57d1485e3b3db41eb4c9850793682a5110fcd4
Artem Starovoitov <myanowitch@yandex.ru>
2024-03-09 02:17:46 +0300

    такая поебень огогогогго

---
+++ кастыль которым я горжусь/output.js
@@ -0,0 +1,54 @@
+let json= [
+    {
+        "id": 1,
+        "name": "BEBRA",
+        "category": "books",
+        "description": "xz",
+        "promo": 123
+    },
+    {
+        "id": 2,
+        "name": "iVAN",
+        "category": "pidoras",
+        "description": "xz",
+        "promo": 321
+    },
+    {
+        "id": 3,
+        "name": "фывыфв",
+        "category": "who",
+        "description": "xz",
+        "promo": 213
+    },
+    {
+        "id": 4,
+        "name": "123",
+        "category": "321",
+        "description": "13",
+        "promo": 123
+    },
+    {
+        "id": 5,
+        "name": "123",
+        "category": "123",
+        "description": "чя",
+        "promo": "выыаыф"
+    },
+    {
+        "id"    : 6,
+        "name": "павып",
+        "category": "ыфпввп",
+        "description": "фывп",
+        "promo": null
+    },
+    {
+        "id": 7,
+        "name": null,
+        "category": null,
+        "description": null,
+        "promo": null
+    }
+];
+
+window.json = json;
+
